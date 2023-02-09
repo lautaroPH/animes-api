508c59287a578df1d29c02a2c506deea86e209b0
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-02-09 17:30:40 -0300

    remove console logs

--- utils/supabase-client.js
+++ utils/supabase-client.js
@@ -1,11 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0';
-import { load } from 'https://deno.land/std@0.177.0/dotenv/mod.ts';
-
-const env = await load();
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL');
 const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY');
-
-console.log(supabaseAnonKey, supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
