a52724fa7a9782b8338d32f4bcaddeebf05ed4f4
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-02-09 17:20:10 -0300

    fix bug

--- utils/supabase-client.js
+++ utils/supabase-client.js
@@ -6,7 +6,6 @@ const env = await load();
 const supabaseUrl = env['SUPABASE_URL'];
 const supabaseAnonKey = env['SUPABASE_ANON_KEY'];
 
-export const supabase = createClient(
-  'https://mocvdkjomupgrvizemzh.supabase.co',
-  supabaseAnonKey,
-);
+console.log(supabaseAnonKey, supabaseUrl);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
